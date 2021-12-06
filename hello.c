7f3149311e35eb8b0cd4f20c0017b9b918914ff1
haru0707 <95564227+haru0707@users.noreply.github.com>
2021-12-06 19:02:16 +0900

    korettukannsuu
    コンパイラしてないからどこかおかしいはず

--- hello.c
+++ hello.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int no;
+    
+    printf("整数を入力してください：");
+    scanf("%d", &no);
+    
+    if (no % 2 );
+    int no * 3 + 1;
+    return (0);
+    if (no % 2 != 0);
+    int ( no / 2);
+    return (0);
+    int (no==1); 
+    praintf("seiritusimasu")
+
+  
+}
